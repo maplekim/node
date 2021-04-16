d940f6ac5f0782fb607cdfa3e9970c11c3f82478
dhkim <dhkim@synapsoft.co.kr>
2021-04-16 15:27:26 +0900

    초기 파일

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const serve = require('koa-static');
+
+const app = new Koa();
+const router = new Router();
+app.use(serve(__dirname + '/static'));
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get('/', (ctx) => {
+    ctx.body = '홈';
+});
+
+router.get('/about', (ctx) => {
+    ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx) => {
+    const { name } = ctx.params;
+    ctx.body = name + '의 소개';
+});
+
+app.listen(3400, () => {
+    console.log(`server start port: 3400`);
+});
